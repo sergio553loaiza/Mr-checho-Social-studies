3a05331a6580451ced40c3f4c4287788357b011f
sergio553loaiza <sergio553.loaiza@montessori.edu.co>
2026-09-10 10:15:58 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Mr-checho-Social-studies/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
